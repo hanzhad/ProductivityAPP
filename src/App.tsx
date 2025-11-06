5205d55b2555dd320c7fc5ce098b3e74738f50a3
Hanzha Dmytro <hanzhad732@gmail.com>
2025-11-06 22:33:26 +0200

    Refactor: Add `TasksView` to App layout and improve grid styling in main section. Remove redundant padding from Calendar.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import { Component } from 'solid-js';
 import Calendar from './components/Calendar';
 import Header from './components/Header';
+import TasksView from './components/TasksView';
 
 const App: Component = () => {
   return (
     <div class="flex flex-col min-h-screen bg-gray-50 dark:bg-gray-900 relative">
       <Header />
       <main class="pt-4">
-        <div class="grid grid-cols-1 md:grid-cols-[auto_min(320px)]">
-          {/*<div>*/}
+        <div class="grid grid-cols-1 md:grid-cols-[auto_min(320px)] gap-4 px-4">
           <Calendar />
-          {/*<TasksView />*/}
+          <TasksView />
         </div>
       </main>
     </div>
--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -51,7 +51,7 @@ const Calendar: Component = () => {
   };
 
   return (
-    <div class="max-w-7xl px-4">
+    <div class="max-w-7xl">
       <CalendarView />
     </div>
   );
